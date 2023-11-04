a15cf30da2b214ba12c0f7b5defd90fa38824d8b
Startive <140018802+Startive@users.noreply.github.com>
2023-11-04 14:29:33 +0000

    Add files via upload

---
+++ bypass/FPS Bypass 1.9/FPS Bypass 1.9.cpp
@@ -0,0 +1,22 @@
+﻿// FPS Bypass 1.9.cpp : Defines the entry point for the application.
+//
+
+#include "FPS Bypass 1.9.h"
+
+
+int main()
+{
+	DWORD fps;
+
+	cout << "Not actually bypass yet, just an attempt spoofer." << endl;
+
+	cout << "what is blud waffalin about ?!?!: ";
+	cin >> fps;
+	cout << '\n';
+
+	cout << "Wow.. " << fps << " fps??" << endl;
+	cout << "amazing" << endl;
+
+	system("pause");
+	return 0;
+}
---
+++ bypass/FPS Bypass 1.9/FPS Bypass 1.9.h
@@ -0,0 +1,10 @@
+﻿// FPS Bypass 1.9.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+#include <Windows.h>
+
+using std::cout, std::cin, std::endl;
